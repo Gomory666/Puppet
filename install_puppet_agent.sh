c86c9b0f4b5b80ce230a451270f2e0f11e7fc194
fmourterou <fmourterou@talentsoft.com>
2019-01-15 10:09:24 +0100

    add sudo

--- install_puppet_agent.sh
+++ install_puppet_agent.sh
@@ -2,23 +2,23 @@
 ######################################### Puppet CLIENT #########################################
 
 ## Nameserver
-echo nameserver 8.8.8.8 > /etc/resolv.conf
+sudo echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 #Stop resolvconf
 sudo systemctl stop service.resolvconf
 
 #Install Puppet agent
-    curl -O https://apt.puppetlabs.com/puppet5-release-xenial.deb 
-    sudo dpkg -i puppet5-release-xenial.deb
-    sudo apt-get update
-	sudo apt install -y puppet-agent
+sudo curl -O https://apt.puppetlabs.com/puppet5-release-xenial.deb 
+sudo dpkg -i puppet5-release-xenial.deb
+sudo apt-get update
+sudo apt install -y puppet-agent
 	
 #IP + HOSTNAME dans /etc/hosts
-echo "$(ip -o -4 a|sed -n 's#.*inet\s*\([^ ]*\)/.*brd.*$#\1#p') $(hostname)" >> /etc/hosts
+sudo echo "$(ip -o -4 a|sed -n 's#.*inet\s*\([^ ]*\)/.*brd.*$#\1#p') $(hostname)" >> /etc/hosts
 
 
 #Puppet conf clients with echo
-echo -e "[agent] \n default_schedules = false \n report = true \n pluginsync = true \n masterport = 8140 \n environment = production \n certname = $(hostname) \n server = $1 \n listen = false \n splay = false \n splaylimit = 1800 \n runinterval = 1800 \n noop = false \n usecacheonfailure = true" > /etc/puppetlabs/puppet/puppet.conf
+sudo echo -e "[agent] \n default_schedules = false \n report = true \n pluginsync = true \n masterport = 8140 \n environment = production \n certname = $(hostname) \n server = $1 \n listen = false \n splay = false \n splaylimit = 1800 \n runinterval = 1800 \n noop = false \n usecacheonfailure = true" > /etc/puppetlabs/puppet/puppet.conf
 
 #Run and Enable puppet-agent
 sudo systemctl start puppet.service
